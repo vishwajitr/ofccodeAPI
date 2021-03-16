02b974253867405c5e6bd68bd08e2d04879bd7ec
vishwajit.r <vishwajitrikame@gmail.com>
2021-03-16 12:46:00 +0530

    unwanted files removed and kw search fun fixed my cors middleware

--- pages/api/front/search/kws__by__query.js
+++ pages/api/front/search/kws__by__query.js
@@ -1,10 +1,35 @@
 import { keywords } from '../../../../keywords__data'
 // http://localhost:3002/api/front/search/kw__search?q=myntra
 const keywordSet = keywords
-export default (req, res) => {
+
+import Cors from 'cors'
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
   const results = req.query.q ?
   keywordSet.filter(keyword => keyword.keyword.toLowerCase().includes(req.query.q))  : []
   res.status(200).json({ results })
 }
 
 
+export default handler
--- pages/api/front/search/stores__by__query.js
+++ pages/api/front/search/stores__by__query.js
@@ -1,9 +1,32 @@
 import { stores } from '../../../../stores__data'
 const posts = stores
 // http://localhost:3002/api/front/search/stores__by__query?q=myn
+import Cors from 'cors'
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
 
-export default (req, res) => {
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
   const results = req.query.q ?
     posts.filter(post => post.name.toLowerCase().includes(req.query.q))  : []
   res.status(200).json({ results })
 }
+
+export default handler
