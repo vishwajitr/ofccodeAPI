1e1ae74edf34e239ee0e378415346ebfaae09bc4
vishwajit.r <vishwajitrikame@gmail.com>
2021-10-30 13:20:51 +0530

    api changes

--- pages/api/front/cuels/cuelOffers.js
+++ pages/api/front/cuels/cuelOffers.js
@@ -1,6 +1,16 @@
 import amazon__offfers from '../../../../stores/amazon/api__coupons.json';
 import flipkart__offers from '../../../../stores/flipkart/api__coupons.json';
 import myntra__offers from '../../../../stores/myntra/api__coupons.json';
+import ajio__offers from '../../../../stores/ajio/api__coupons.json';
 
-const Offers = amazon__offfers.concat(flipkart__offers, myntra__offers)
+shuffle(amazon__offfers);
+shuffle(flipkart__offers);
+shuffle(myntra__offers);
+shuffle(ajio__offers);
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+const Offers = amazon__offfers.concat(flipkart__offers, myntra__offers,ajio__offers)
 module.exports = Offers
