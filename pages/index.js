7d2f54dab4dfe67cbfc4a70a074a248b1de6dba2
vishwajit.r <vishwajit.r@MMMCDVISHWAJITRI.local>
2022-06-14 20:32:28 +0530

    read me doc added

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+function Index() {
+    return <div>
+        Welcome to OffersCode Api
+        <br/> <br/>
+        Read Me Doc <br/>
+        <Link href=" https://github.com/vishwajitr/ofccodeAPI#readme">
+          <a> https://github.com/vishwajitr/ofccodeAPI#readme</a>
+        </Link>
+    </div>
+  }
+  
+  export default Index
